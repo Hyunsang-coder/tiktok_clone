9d1bcc5dfc5499ff479a866eb266ecdd76aa4ec1
HyunsangJoo <joohs86@naver.com>
2024-04-03 18:32:40 +0900

    AnimationController

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -16,9 +16,15 @@ class VideoPost extends StatefulWidget {
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
+class _VideoPostState extends State<VideoPost> with TickerProviderStateMixin {
   final VideoPlayerController _videoController =
       VideoPlayerController.asset('assets/videos/subway.mp4');
+
+  late final AnimationController _animationController;
+
+  bool isPaused = false;
+
+  final Duration _animationDuration = const Duration(milliseconds: 200);
 
   _initVideoPlayer() async {
     await _videoController.initialize();
@@ -39,6 +45,14 @@ class _VideoPostState extends State<VideoPost> {
   void initState() {
     super.initState();
     _initVideoPlayer();
+
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 1.0,
+      upperBound: 1.5,
+      value: 1.5,
+      duration: _animationDuration,
+    );
   }
 
   @override
@@ -48,19 +62,23 @@ class _VideoPostState extends State<VideoPost> {
   }
 
   void _onVisibilityChanged(VisibilityInfo info) {
-    print("Screen ${widget.index} ==> ${info.visibleFraction}");
+    //print("Screen ${widget.index} ==> ${info.visibleFraction}");
     if (info.visibleFraction == 1 && !_videoController.value.isPlaying) {
       _videoController.play();
     }
   }
 
   void _onTogglePause() {
+    if (_videoController.value.isPlaying) {
+      _videoController.pause();
+      _animationController.reverse();
+    } else {
+      _videoController.play();
+      _animationController.forward();
+    }
+
     setState(() {
-      if (_videoController.value.isPlaying) {
-        _videoController.pause();
-      } else {
-        _videoController.play();
-      }
+      isPaused = !isPaused;
     });
   }
 
@@ -82,12 +100,18 @@ class _VideoPostState extends State<VideoPost> {
               child: GestureDetector(
             onTap: _onTogglePause,
           )),
-          if (!_videoController.value.isPlaying)
-            const Positioned.fill(
-                child: IgnorePointer(
-              child: Icon(FontAwesomeIcons.play,
-                  size: Sizes.size52, color: Colors.white),
-            ))
+          Transform.scale(
+            scale: _animationController.value,
+            child: AnimatedOpacity(
+              opacity: isPaused ? 1 : 0,
+              duration: _animationDuration,
+              child: const Center(
+                  child: IgnorePointer(
+                child: Icon(FontAwesomeIcons.play,
+                    size: Sizes.size52, color: Colors.white),
+              )),
+            ),
+          )
         ],
       ),
     );
